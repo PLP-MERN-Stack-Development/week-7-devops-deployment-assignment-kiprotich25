9b032334bdda6f9eb6371bf2aee08e03c2d72598
ian254-byte <iankipro273@gmail.com>
2025-07-20 12:19:45 +0300

    complete assignment

--- FarmSoko/backend/controllers/authController.js
+++ FarmSoko/backend/controllers/authController.js
@@ -1,7 +1,7 @@
 const bcrypt = require ('bcrypt')
 const jwt = require('jsonwebtoken')
 const User = require("../models/User");
-//const { generateToken } = require('../middleware/auth')
+
 
 exports.register = async (req, res) => {
     const { username, email, password, role } = req.body;
@@ -27,17 +27,6 @@ exports.register = async (req, res) => {
           token,
         });
 
-        
-        // if (newUser) {
-        //     res.status(201).json({
-        //         _id: newUser._id,
-        //         username: newUser.username,
-        //         email: newUser.email,
-        //         role: newUser.role,
-        //         token: generateToken(newUser._id),
-        //     })
-        // }
-        
         
     } catch (error) {
         console.error(error)
